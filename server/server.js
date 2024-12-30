51d7046cd65dde6fe5cf62269e99b3911fbcc1b5
adam-dev2 <shaikadam252@gmail.com>
2024-12-30 19:04:08 +0530

    done with user signup/login with authentication

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt')
+
 const port = 3000;
 require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 
 const DB_URL = process.env.DB_URL;
 
@@ -38,7 +42,49 @@ db.once('open',() => {
     console.log("mongo db connected");
 })
 
+app.post('/user/signup',async(req,res) => {
+    try{
+        const {name,email,password} = req.body;
+        const usercheck = await User.findOne({email});
 
+        if(usercheck) {
+            return res.status(400).json({message: 'User already exists with this email'});
+        }
+        const hashedpassword = await bcrypt.hash(password,10);
+
+        const newUser = new User({
+            name,
+            email,
+            password: hashedpassword,
+        })
+
+        await newUser.save();
+
+        res.status(201).json({message: 'User created succefully'});
+    }catch(err) {
+        res.status(500).json({message: 'Server Error'});
+    }
+})
+
+app.post('/user/login',async(req,res) => {
+    try {
+        const {email,password} = req.body;
+        const finduser = User.findOne({email});
+        if(!finduser) {
+            return res.status(400).json({message: 'Invalid Email'});
+        }
+
+        const isMatch = await bcrypt.compare(password,finduser.password);
+
+        if(!isMatch) {
+            return res.status(400).json({message: 'Invalid Password'});
+        }
+
+        res.status(200).json({message: 'Login Successful'});
+    } catch(err) {
+        res.status(500).json({message: 'Server Error'});
+    }
+})
 
 app.listen(port,() => {
     console.log("Listetning on 3000");
