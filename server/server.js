e604a0dc7a816e59a0bf7b84cf39020089826ab2
adam-dev2 <shaikadam252@gmail.com>
2024-12-30 19:32:47 +0530

    completed the setup of middleware

--- server/server.js
+++ server/server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const port = 3000;
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 const app = express();
 
@@ -13,6 +15,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const DB_URL = process.env.DB_URL;
+
 
 mongoose.connect(DB_URL,{
     useNewUrlParser: true,
@@ -41,6 +44,23 @@ const Task = mongoose.model("Task",taskSchema);
 db.once('open',() => {
     console.log("mongo db connected");
 })
+
+const authenticate = (req,res,next) => {
+    const token = req.header("Authorization");
+
+    if(!token) {
+        return res.status(401).json({message: 'Authorization denied'});
+    }
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded.user;
+        next();
+    } catch(err) {
+        console.log(err);
+        return res.status(401).json({message: 'Token is invalid'});
+    }
+}
 
 app.post('/user/signup',async(req,res) => {
     try{
@@ -80,9 +100,23 @@ app.post('/user/login',async(req,res) => {
             return res.status(400).json({message: 'Invalid Password'});
         }
 
-        res.status(200).json({message: 'Login Successful'});
+        const token = jwt.sign(
+            {userId: finduser._id},
+            process.env.JWT_SECRET,
+            {expiresIn: '1h'}
+        );
+
+        res.status(200).json({message: 'Login Successful',token});
     } catch(err) {
-        res.status(500).json({message: 'Server Error'});
+        return res.status(500).json({message: 'Server Error'});
+    }
+})
+
+app.post('/task/newtask',authenticate,async(req,res) => {
+    try {
+
+    }catch(err) {
+        return res.status(500).json({message: 'Server Error'});
     }
 })
 
